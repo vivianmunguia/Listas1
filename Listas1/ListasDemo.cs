2b80ad0501b258490116729f157f8af6eece3ace
Vivian Munguía <andy_munguia@hotmail.com>
2022-06-23 21:00:52 -0700

    A list was created using an array, then it was filtered with linq

---
+++ Listas1/ListasDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+namespace Listas1
+{
+    public class ListasDemo : ContentPage
+    {
+        public ListasDemo()
+        {
+            var nombres = new[]
+            {
+                "Héctor",
+                "Cristina",
+                "Regina",
+                "Juan",
+                "Ronaldo",
+                "Humberto",
+                "Hopkins",
+                "Cristiano",
+                "César"
+            };
+
+            var miListView = new ListView();
+            miListView.ItemsSource = from nombre in nombres
+                                     where nombre.StartsWith("H")
+                                     select nombre;
+            Content = miListView;
+        }
+    }
+}
